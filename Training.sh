0f1507d1cf018fd35d796e15bb9f3f0c6fef9cb1
garcia-nacho <garcia.nacho@gmail.com>
2022-04-12 08:40:11 +0200

    Fixed training step

--- Training.sh
+++ Training.sh
@@ -6,8 +6,8 @@ else
 
 if [[ "$(docker images -q precfinder 2> /dev/null)" != "" ]] 
 then
-  mkdir {1}
-  docker run -it --rm -v $(pwd)/Models:/Models -v $(pwd)/Training:/Training precfinder Rscript /home/docker/Scripts/Training.R {1}
+  mkdir Models/${1}
+  docker run -it --rm -v $(pwd)/Models:/Models -v $(pwd)/Training:/Training precfinder Rscript /home/docker/Scripts/Training.R ${1}
   echo "Adding your model to the image..."
   docker build -t precfinder .
 else
